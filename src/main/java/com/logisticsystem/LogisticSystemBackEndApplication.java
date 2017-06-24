0ab26b36920bd7a7c157a73fa6da91bcf3f5dc02
denisolkhovik <denis.olkhovik42@gmail.com>
2017-06-24 22:36:08 +0300

    Init commit

---
+++ src/main/java/com/logisticsystem/LogisticSystemBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.logisticsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticSystemBackEndApplication.class, args);
+	}
+}
